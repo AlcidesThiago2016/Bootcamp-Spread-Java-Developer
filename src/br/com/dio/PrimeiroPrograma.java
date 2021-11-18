87f984a8fa9fce1eb6bf0ffe505a51d968d47f28
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2021-11-17 22:46:08 -0300

    Criando Class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,6 +2,8 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         /*int a = 5;
@@ -11,5 +13,56 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
 
         System.out.println(gato);
+
+        Livro livro = new Livro("O Problema do 3 Corpos", 300);
+
+        System.out.println(livro);
+    }
+}
+
+class Livro {
+    private String nome;
+    private  Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(npag, livro.npag);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, npag);
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
     }
 }
