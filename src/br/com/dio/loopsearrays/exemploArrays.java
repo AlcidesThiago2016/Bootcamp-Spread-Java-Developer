20dc24475e35bbee58bee4abdd6544bc7b658a07
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2021-12-09 21:04:57 -0300

    Criando Class exemploArrays

---
+++ src/br/com/dio/loopsearrays/exemploArrays.java
@@ -0,0 +1,30 @@
+package br.com.dio.loopsearrays;
+/*
+    PROGRAMA COM EXEMPLOS DE ARRAYS
+ */
+
+public class exemploArrays {
+    public static void main(String[] args) {
+
+        int[] idades = new int[10];
+        idades[0] = 27;
+        idades[1] = 31;
+        System.out.println("Idade 0: " + idades[0]);
+        System.out.println("Idade 1: " + idades[1]);
+        System.out.println("Idade 2: " + idades[2]);
+
+        int[] i = {10,25};
+        System.out.println("i 0: " + i[0]);
+        System.out.println("i 1: " + i[1]);
+
+        int[] k = new int[] {1,2,3};
+        System.out.println("k 0: " + k[0]);
+        System.out.println("k 1: " + k[1]);
+        System.out.println("k 2: " + k[2]);
+
+        int[] j = new int[2];
+        System.out.println("j 0: "+ j[1]);
+
+
+    }
+}
