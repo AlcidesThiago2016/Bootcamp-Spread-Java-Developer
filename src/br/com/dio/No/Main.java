020afe769de1a68e17d1f100771e36c51b5c5cd5
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2021-12-16 23:06:47 -0300

    Refatoração Classe Main para implementação de Generics

--- src/br/com/dio/No/Main.java
+++ src/br/com/dio/No/Main.java
@@ -3,15 +3,15 @@ package br.com.dio.No;
 public class Main {
     public static void main(String[] args) {
 
-        No no1 = new No("Conteúdo no1");
+        No<String> no1 = new No<>("Conteúdo no1");
 
-        No no2 = new No("Conteúdo no2");
+        No<String> no2 = new No<>("Conteúdo no2");
         no1.setProximoNo(no2); //NÓ 1 APONTANDO PARA O NÓ 2
 
-        No no3 = new No("Conteúdo no3");
+        No<String> no3 = new No<>("Conteúdo no3");
         no2.setProximoNo(no3);//NÓ 2 APONTANDO PARA O NÓ 3
 
-        No no4 = new No("Conteúdo no4");
+        No<String> no4 = new No<>("Conteúdo no4");
         no3.setProximoNo(no4);//NÓ 3 APONTANDO PARA O NÓ 4
         //QUANDO ACABA OS APONTAMENTOS O ULTIMO NÓ APONTA PARA NULL
 
