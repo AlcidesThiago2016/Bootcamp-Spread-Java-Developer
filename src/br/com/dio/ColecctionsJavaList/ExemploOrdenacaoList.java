bd93de701ee4fe034168b32ef23e01e5306eb081
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2021-12-27 12:25:43 -0300

    Implementação de Lista com itens e metodo toString()

---
+++ src/br/com/dio/ColecctionsJavaList/ExemploOrdenacaoList.java
@@ -0,0 +1,60 @@
+package br.com.dio.ColecctionsJavaList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExemploOrdenacaoList {
+    public static void main(String[] args) {
+
+        List<Gato> meusGatos = new ArrayList<>(){{
+           add(new Gato("Jon", 18,"preto"));
+           add(new Gato("Simba", 6,"tigrado"));
+           add(new Gato("Jon", 12,"amarelo"));
+        }};
+    }
+}
+
+class Gato{
+    private String nome;
+    private Integer idade;
+    private String cor;
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", cor='" + cor + '\'' +
+                '}';
+    }
+}
