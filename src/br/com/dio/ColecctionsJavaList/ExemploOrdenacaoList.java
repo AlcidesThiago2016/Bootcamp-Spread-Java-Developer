422f328c83fec482cc04e531aa41e4c30f3aada8
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2021-12-27 12:19:50 -0300

    Implementação classe ExemploOrdenacaoList e Classe Gato

---
+++ src/br/com/dio/ColecctionsJavaList/ExemploOrdenacaoList.java
@@ -0,0 +1,45 @@
+package br.com.dio.ColecctionsJavaList;
+
+public class ExemploOrdenacaoList {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+class Gato{
+    private String nome;
+    private Integer idade;
+    private String cor;
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+}
